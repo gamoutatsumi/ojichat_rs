2c7b72a24758437855093233172be9d77deb95d1
gamoutatsumi <47162587+gamoutatsumi@users.noreply.github.com>
2020-12-10 23:53:58 +0900

    setup clap

---
+++ src/generator/mod.rs
@@ -0,0 +1,5 @@
+pub struct Config {
+    target_name: String,
+    emoji_num: i64,
+    punctuation_level: i64
+}
---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use clap::*;
+mod generator;
+
+fn main() {
+    let app = App::new(crate_name!())
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about(crate_description!())
+        .arg(Arg::from_usage("-e [number] '絵文字/顔文字の最大連続数 [default: 4]'"))
+        .arg(Arg::from_usage("-p [level] '句読点挿入頻度レベル [min:0, max:3] [default: 0]'"))
+        .arg(Arg::from_usage("[name] 'おじさんのメール相手'"))
+        .setting(AppSettings::ColoredHelp);
+
+    let matches = app.get_matches();
+
+    if let Some(o) = matches.value_of("p") {
+        println!("Value for level: {}", o)
+    };
+
+    if let Some(o) = matches.value_of("name") {
+        println!("Value for name: {}", o)
+    };
+
+}
